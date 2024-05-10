2ddabd1144484759004b4e46d7604a88586c5e56
Linh Phạm <phamlinh0208@gmail.com>
2024-05-10 10:56:43 +0700

    add tiktok v2 - api v202309

---
+++ src/TikTokRequestV2.php
@@ -0,0 +1,81 @@
+<?php
+namespace TikTok;
+
+use Exception;
+
+class TikTokRequestV2
+{
+	public $apiName;
+
+	public $headerParams = array();
+
+	public $udfParams = array();
+	public $bodyData = array();
+
+	public $fileParams = array();
+
+	public $httpMethod = 'POST';
+
+	public function __construct($apiName,$httpMethod = 'POST')
+	{
+		$this->apiName = $apiName;
+		$this->httpMethod = $httpMethod;
+
+		if($this->startWith($apiName,"//"))
+		{
+			throw new Exception("api name is invalid. It should be start with /");
+		}
+	}
+    function setBodyData($body)
+    {
+        $this->bodyData = $body;
+    }
+
+
+	function addApiParam($key,$value)
+	{
+
+		if(!is_string($key))
+		{
+			throw new Exception("api param key should be string");
+		}
+
+		$this->udfParams[$key] = $value;
+	}
+
+	function addFileParam($key,$content,$mimeType = 'application/octet-stream')
+	{
+		if(!is_string($key))
+		{
+			throw new Exception("api file param key should be string");
+		}
+
+		$file = array(
+            'type' => $mimeType,
+            'content' => $content,
+            'name' => $key
+        );
+		$this->fileParams[$key] = $file;
+	}
+
+	function addHttpHeaderParam($key,$value)
+	{
+		if(!is_string($key))
+		{
+			throw new Exception("http header param key should be string");
+		}
+
+		if(!is_string($value))
+		{
+			throw new Exception("http header param value should be string");
+		}
+
+		$this->headerParams[$key] = $value;
+	}
+
+	function startWith($str, $needle) {
+	    return strpos($str, $needle) === 0;
+	}
+}
+
+?>
