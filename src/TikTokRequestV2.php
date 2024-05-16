6a76702a74fc8d47d16e98bf25de7e4d0168c8f8
quanglx <quang268200@gmail.com>
2024-05-16 14:37:32 +0700

    update set bodyData null

--- src/TikTokRequestV2.php
+++ src/TikTokRequestV2.php
@@ -10,7 +10,7 @@ class TikTokRequestV2
 	public $headerParams = array();
 
 	public $udfParams = array();
-	public $bodyData = array();
+	public $bodyData = null;
 
 	public $fileParams = array();
 
